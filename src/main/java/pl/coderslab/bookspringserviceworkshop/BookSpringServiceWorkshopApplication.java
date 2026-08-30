19493632ecd46f387a239035f746602d0de7412e
klau44 <ksawa@visa.com>
2026-08-30 18:22:27 +0200

    init commit

---
+++ src/main/java/pl/coderslab/bookspringserviceworkshop/BookSpringServiceWorkshopApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.bookspringserviceworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSpringServiceWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSpringServiceWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/bookspringserviceworkshop/BookSpringServiceWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.bookspringserviceworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpringServiceWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
